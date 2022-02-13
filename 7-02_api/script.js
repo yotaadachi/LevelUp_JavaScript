2bcbdb6688487ff04a3324b69bcaa7403bcf936a
yotaadachi <yota961219@gmail.com>
2022-02-13 14:55:26 +0900

    7-2演習

--- 7-02_api/script.js
+++ 7-02_api/script.js
@@ -2,7 +2,7 @@
 
 // geolocation
 function success(pos) {
-  ajaxRequest(pos.latitude, pos.coords.longitude);
+  ajaxRequest(pos.coords.latitude, pos.coords.longitude);
 }
 
 function fail(error) {
@@ -13,7 +13,7 @@ navigator.geolocation.getCurrentPosition(success, fail);
 
 // データ取得
 function ajaxRequest(lat, long) {
-  const url = 'https://api.openweathermap.org/data/2.5/forcast';
+  const url = 'https://api.openweathermap.org/data/2.5/forecast';
   const appId = '3d712a8f72596544d72e0872675b3156';
 
   $.ajax ({
